ec891c51022ac52991fd7f23141b70f306e46425
105normal <hamer.tv@outlook.com>
2022-11-10 10:34:08 +0100

    podstawowy ViewModel

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aplikacja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -10,8 +11,9 @@ import com.example.aplikacja.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Dzieki binding mozna nei uzywac findViewById
     private ActivityMainBinding binding;
-    private int punkty = 0;
+    PunktyViewModel punktyViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
+
+        binding.text.setText(Integer.toString(punktyViewModel.getPunkty()));
+        //Wazna kolejnosc najpierw tworzyszpozniej robisz punkjty
+
+
         binding.button.setOnClickListener(view1 -> {
-            punkty++;
-            binding.text.setText(String.valueOf(punkty)); //value of zamiast to stringa bo nie ma Integera
+            punktyViewModel.dodajPunkty(1);
+            binding.text.setText(Integer.toString(punktyViewModel.getPunkty())); //value of zamiast to stringa bo nie ma Integera
+        });
+        binding.button2.setOnClickListener(view1 -> {
+            punktyViewModel.dodajPunkty(2);
+            binding.text.setText(Integer.toString(punktyViewModel.getPunkty())); //value of zamiast to stringa bo nie ma Integera
+        });
+        binding.button3.setOnClickListener(view1 -> {
+            punktyViewModel.dodajPunkty(3);
+            binding.text.setText(Integer.toString(punktyViewModel.getPunkty())); //value of zamiast to stringa bo nie ma Integera
         });
     }
+    //Anty znikacz
 }

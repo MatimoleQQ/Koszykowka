374ac0c6ff8b05bf1628d5296c021e15adeecde7
105normal <hamer.tv@outlook.com>
2022-11-10 10:12:47 +0100

    bindowanie widokow i przycisk +1

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.aplikacja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.aplikacja.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private int punkty = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.button.setOnClickListener(view1 -> {
+            punkty++;
+            binding.text.setText(String.valueOf(punkty)); //value of zamiast to stringa bo nie ma Integera
+        });
     }
 }
